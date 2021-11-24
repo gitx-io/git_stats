5fa830aa230c141450bbfae4dc007aa64ee6c6a7
B1nj0y <idegorepl@gmail.com>
2021-11-24 14:34:04 +0800

    Update workflow

--- generate.sh
+++ generate.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Rails
-git clone git@github.com:rails/rails.git
-# jQuery
-git clone git@github.com:jquery/jquery.git
-# Golang
-git clone git@github.com:golang/go.git
-
-
 for repo_path in ./test_repos/*; do
   project_name="$(basename $repo_path)"
   output_path=$(readlink -m ./examples/$project_name)
