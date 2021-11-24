f59a560c27a7d7d2544a30baff5326a3d2084b35
B1nj0y <idegorepl@gmail.com>
2021-11-24 13:52:43 +0800

    Add test repos

--- generate.sh
+++ generate.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Rails
+git clone git@github.com:rails/rails.git
+# jQuery
+git clone git@github.com:jquery/jquery.git
+# Golang
+git clone git@github.com:golang/go.git
+
+
 for repo_path in ./test_repos/*; do
   project_name="$(basename $repo_path)"
   output_path=$(readlink -m ./examples/$project_name)
