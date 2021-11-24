7f63e53672d007d9deaa358ed13b38b023532567
B1nj0y <idegorepl@gmail.com>
2021-11-24 15:39:50 +0800

    Reduce test repos

--- generate.sh
+++ generate.sh
@@ -4,5 +4,6 @@ for repo_path in ./test_repos/*; do
   project_name="$(basename $repo_path)"
   output_path=$(readlink -m ./examples/$project_name)
 
-  git_stats generate -p $repo_path -o $output_path
+  echo "-----------------> $project_name"
+  git_stats generate -p $repo_path -o $output_path --silent
 done
